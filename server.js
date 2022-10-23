6ca3e449ffb8ed8e10e9dfc88e57a3ef54fbbdf7
sebhastiao-lucio <sebastiao@ice.ufjf.br>
2022-10-23 09:08:46 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+/*
+*@autor: Rio 3D Studios
+*@description:  java script server that works as master server of the Basic Example of WebGL Multiplayer Kit
+*/
+var express  = require('express');//import express NodeJS framework module
+var app      = express();// create an object of the express module
+var http     = require('http').Server(app);// create a http web server using the http library
+var io       = require('socket.io')(http);// import socketio communication module
+
+
+app.use("/public/TemplateData",express.static(__dirname + "/public/TemplateData"));
+app.use("/public/Build",express.static(__dirname + "/public/Build"));
+app.use(express.static(__dirname+'/public'));
+
+
+//open a connection with the specific client
+io.on('connection', function(socket){
+
+   //print a log in node.js command prompt
+  console.log('A user ready for connection!');
+ 
+		
+});//END_IO.ON
+
+
+http.listen(process.env.PORT ||3000, function(){
+	console.log('listening on *:3000');
+});
+console.log("------- server is running -------");
